a7bb3642695976e5128f91c5f216108026b51f1b
rodrigo ozores <glario@MacBook-Pro-de-rodrigo.local>
2018-05-10 21:28:24 +0200

    P5_Quiz

---
+++ routes/index.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Quiz' });
+});
+
+router.get('/credits', function(req, res, next) {
+  res.render('credits');
+});
+
+router.get('/quizzes', function(req, res, next) {
+  models.quiz.findAll().
+  then(quizzes=>{
+  res.render('quizzes', { quizzes, title: "Quizzes" });})
+});
+
+module.exports = router;
