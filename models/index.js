18c04c707bb981ae4f8ad232053096a430d8de3e
rodrigo ozores <glario@MacBook-Pro-de-rodrigo.local>
2018-05-11 10:19:13 +0200

    P5_Quiz

--- models/index.js
+++ models/index.js
@@ -1,112 +1,33 @@
-const readline = require('readline');
+const Sequelize = require ('sequelize');
 
-const {log, biglog, errorlog, colorize} = require("./out");
-const cmds = require("./cmds");
+const sequelize = new Sequelize("sqlite:quizzes.sqlite", {logging: false});
 
-const net = require("net");
-
-net.createServer(socket => {
-
-  console.log("Se ha conectado un cliente desde" + socket.remoteAddress);
-// mensaje inicial
-biglog (socket, 'CORE Quiz', 'green');
-
-const rl = readline.createInterface({
-  input: socket,
-  output: socket,
-  prompt: colorize("quiz >", 'blue'),
-  completer : (line) => {
-  const completions = 'h help add delete edit list test p play credits q quit'.split(' ');
-  const hits = completions.filter((c) => c.startsWith(line));
-  // show all completions if none found
-  return [hits.length ? hits : completions, line];
-}
-
-});
-
-socket
-.on("end" , () => {rl.close();})
-.on("error" , () => {rl.close();})
-
-rl.prompt();
-
-rl.on('line', (line) => {
-  
-  let args = line.split(" ");
-  let cmd = args[0].toLowerCase().trim();
-  
-  
-  
-  
-  switch (cmd) {
-    case  ' ':
-       rl.prompt();
-      break;
-      
-    case 'h':
-    case 'help':
-      cmds.helpCmd(socket, rl);
-      break;
-      
-    case 'quit':
-    case 'q':
-     cmds.quitCmd(socket, rl);
-      break;
-      
-    case 'add':
-      cmds.addCmd(socket, rl);
-      
-      break;
-      
-    case 'list':
-      cmds.listCmd(socket, rl);
-      
-      break;
-      
-      
-    case 'show':
-      cmds.showCmd(socket, rl, args[1]);
-      
-      break;
-      
-    case 'test':
-      cmds.testCmd(socket, rl, args[1]);
-      
-      break;   
-    
-    case 'play':
-    case  'p':
-      cmds.playCmd(socket, rl);
-      
-      break;
-      
-    case 'delete':
-      cmds.delCmd(socket, rl, args[1]);
-      
-      break;
-      
-    case 'edit':
-      cmds.editCmd(socket, rl, args[1]);
-      
-      break;
-      
-    case 'credits':
-      cmds.creditsCmd(socket, rl);
-  
-      break;  
-      
-    default:
-      log (socket, `Comando desconocido: '${colorize(cmd, 'red')}'`);
-      log(socket, `Use ${colorize('help' , 'green')} para ver todos los comandos disponibles.`);
-       rl.prompt();
-      break;
+sequelize.define('quiz', {
+  question: {
+    type: Sequelize.STRING,
+    unique: {msg: "Ya existe esta pregunta"},
+    validate: {notEmpty: {msg: "La pregunta no puede estar vacía"}}
+  },
+  answer: {
+    type: Sequelize.STRING,
+    validate: {notEmpty: {msg: "La respuesta no puede estar vacía"}}
   }
-  
- 
-}).on('close', () => {
-  console.log(socket, 'Adiós');
-  
 });
 
+sequelize.sync()
+.then(() => sequelize.models.quiz.count())
+.then(count => {
+  if (!count) {
+    return sequelize.models.quiz.bulkCreate([
+      {question: "Capital de Italia", answer: "Roma"},
+      {question: "Capital de Francia", answer: "Paris"},
+      {question: "Capital de España", answer: "Madrid"},
+      {question: "Capital de Potugal", answer: "Lisboa"},
+    ]);
+  }
 })
-.listen(3030);
+.catch(error =>{
+  console.log(error);
+});
+
+module.exports = sequelize;
